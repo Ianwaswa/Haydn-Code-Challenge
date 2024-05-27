1c219e578b225408fa090b177c6b6597d0debce2
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-27 16:45:51 +0300

    update code

--- print_another_pine_tree.py
+++ print_another_pine_tree.py
@@ -9,9 +9,7 @@ tree_height = int(tree_height)
 spaces = tree_height - 1
 hashes = 1
 tree_stump = tree_height - 1
-
 while tree_height != 0:
-    
     for i in range(spaces):
         print(' ', end="")
     for i in range(hashes):
@@ -19,7 +17,7 @@ while tree_height != 0:
     print()
     spaces -= 1
     hashes += 2
-    tree_stump -= 1
+    tree_height -= 1
 for i in range(tree_stump):
      print(' ', end="")
 print("#")
