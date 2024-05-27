02dd4ff6fb566c9c258d207f4a554f3785654319
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-27 11:48:41 +0300

    update code

--- compound_interest_calculation.py
+++ compound_interest_calculation.py
@@ -8,4 +8,4 @@ money = float(money)
 interest_rate = float(interest_rate) * .01
 for i in range(10):
     money = money + (money * interest_rate)
-    print(round(money, 2))
+print("Investment after 10 years : {:.2f}".format(money))
