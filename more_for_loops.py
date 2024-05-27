bb75d277c3678c4d8b52e0717bf360ff626553d2
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-27 11:12:09 +0300

    update code

---
+++ more_for_loops.py
@@ -0,0 +1,9 @@
+#Use for loop through range 1 to 21
+#Use modulus to check if the result is NOT EQUAL to 0
+#Print the result
+for i in range(1, 21):
+    if ((i % 2) != 0):
+        print("i = ", i, "is odd")
+
+
+
