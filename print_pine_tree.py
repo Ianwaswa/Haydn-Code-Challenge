03a3ff01a8575cde0cf4e201f040477814329b1b
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-27 16:10:12 +0300

    update code

--- print_pine_tree.py
+++ print_pine_tree.py
@@ -1,13 +1,9 @@
-# How long is the tree? 10
-# Use 1 while loop and 3 for loops to print a pine tree
+# Ask how long is the tree
+# Convert the value to integer
+# Decrement spaces by 1 each time you loop through the range
+# Increment the hashes by 2 each time you loop through the range
+# Save spaces to the stump by calculating tree height - 1
 
-tree_height = input("How long is the tree? ")
-
+tree_height = input("How tall is the tree? ")
 tree_height = int(tree_height)
-
-for i in range(tree_height):
-    for j in range(tree_height-i):
-        print(" ", end="")
-    for k in range(2*i+1):
-        print("#", end="")
-    print()
+while tree_height 
