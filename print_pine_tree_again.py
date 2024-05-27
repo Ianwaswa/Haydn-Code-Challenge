69aeaafa27a2193df8d9ee82c07e1007f830d2fe
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-27 16:53:55 +0300

    update code

---
+++ print_pine_tree_again.py
@@ -0,0 +1,22 @@
+# Ask the user to enter their desirable tree height
+# Convert the value to integer
+# Decrement spaces by 1 each time you loop through the range
+# Increment the hashes by 2 each time you loop through the range
+# Save spaces to the stump by calculating tree height - 1
+tree_height = input("How tall do you want the tree to be? ")
+tree_height = int(tree_height)
+spaces = tree_height - 1
+hashes = 1
+tree_stump = tree_height - 1
+while tree_height != 0:
+    for i in range(spaces):
+        print(' ', end="")
+    for i in range(hashes):
+        print('#', end="")
+    print()
+    spaces -= 1
+    hashes += 2
+    tree_height -= 1
+for i in range(tree_stump):
+    print(' ', end="")
+print("#")
