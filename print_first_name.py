006d0775575ec788ff5522f6a5d987d8e7aac6e2
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-28 12:37:43 +0300

    update code

---
+++ print_first_name.py
@@ -0,0 +1,5 @@
+# Use **kwargs function to print users first name
+# Use a minimum of 3 lines code
+def my_function(**name):
+    print("Your first name is " + name["fname"])
+my_function(fname = "Haydn", lname = "Juma")
