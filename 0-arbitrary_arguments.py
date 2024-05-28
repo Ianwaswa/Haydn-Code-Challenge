e906e8b64978b9f977e477030733ec575401d69d
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-28 11:00:12 +0300

    update code

--- 0-arbitrary_arguments.py
+++ 0-arbitrary_arguments.py
@@ -1,4 +1,4 @@
 # Use *args in a function
-def my_kids(*kids):
-    print("The youngest kid of mine is " + kids[0])
-my_kids("Emmilie", "Monalisa", "Lojay")
+def my_kids(child1, child2, child3):
+    print("The youngest kid of mine is " + child3)
+my_kids(child3="Emmilie", child1="Monalisa", child2="Lojay")
