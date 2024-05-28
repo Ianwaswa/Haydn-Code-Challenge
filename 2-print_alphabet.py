c391432d968fc074ab3264fc60c18a582a6a56a7
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-28 09:24:47 +0300

    update code

---
+++ 2-print_alphabet.py
@@ -0,0 +1,7 @@
+# A program that prints the alphabet, in lowercase, not followed by a new line.
+# You can only use one print function with string format
+# You can only use one loop in your code
+# You are not allowed to store characters in a variable
+# You are not allowed to import any module
+for i in range(97, 123):
+    print(chr(i), end="")
