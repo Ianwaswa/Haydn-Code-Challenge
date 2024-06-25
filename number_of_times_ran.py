2d4a49b583861ee8551e4888630f17086594b9ea
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-25 13:48:34 +0300

    Refactor number_of_times_ran.py to optimize calculation of fastest and slowest lap times

--- number_of_times_ran.py
+++ number_of_times_ran.py
@@ -9,4 +9,12 @@ check = True
 for  x in range(1, number_of_laps +1):
     print("Lap ", x)
     time_of_lap = int(input("Enter the time of the lap in minutes: "))
+    while check  ==  True:
+        time_of_fastest_lap = time_of_lap
+        time_of_slowest_lap = time_of_lap
+        check = False
+    if time_of_lap > time_of_slowest_lap:
+        time_of_slowest_lap = time_of_lap
+    if time_of_lap < time_of_fastest_lap:
+        time_of_fastest_lap = time_of_lap
     
