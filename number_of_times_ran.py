f64891c93f15274013a86fd357b5ed11e5e7f178
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-25 13:40:52 +0300

    update code

--- number_of_times_ran.py
+++ number_of_times_ran.py
@@ -2,4 +2,10 @@ number_of_laps = int(input("How many times have you run? "))
 time_of_lap = 0
 time_of_fastest_lap = 0
 time_of_slowest_lap = 0
+total_time_of_laps = 0
+average_time_of_laps = 0
+check = True
+
+for  x in range(1, number_of_laps +1):
+    
     
