e3838b8c54f54eec7789fc6eaa5b4e1d33c80682
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-25 10:22:51 +0300

    Refactor for_loops.py to use a for loop with a step of 2 and include dots in the printed output

--- for_loops.py
+++ for_loops.py
@@ -1,2 +1,8 @@
-for number in range(1, 10, 2):
-    print("Attempt", number, (number) * ".")
+successful = False
+for number in range(3):
+    print("Attempt", number + 1, (number + 1) * ".")
+    if successful:
+        print("Successful")
+        break
+else:
+    print("Unsuccessful")
