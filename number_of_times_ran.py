0aaf535687fbfdb28c63422596ae7d7078eb5e87
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-25 13:32:13 +0300

    update  code

--- number_of_times_ran.py
+++ number_of_times_ran.py
@@ -1,4 +1,5 @@
 number_of_laps = int(input("How many times have you run? "))
-while number_of_laps < 20:
-    time_of_lap = float(input("Enter your lap time: "))
+time_of_lap = 0
+time_of_fastest_lap = 0
+time_of_slowest_lap = 0
     
