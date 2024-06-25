710d34b821ef15644bdadbd2f9136ff036e98589
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-25 13:13:23 +0300

    update  code

---
+++ calories_burned.py
@@ -0,0 +1,6 @@
+calories_burned_per_minute = 4.2
+time = 10
+while time <= 30:
+    calories_burned = calories_burned_per_minute * time
+    print("After", time, "minutes, you've burned", calories_burned, "calories.")
+    time += 5
